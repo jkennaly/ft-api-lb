d75dbff8eaa17232c1ac820f0ec0faf7180d526b
Jason Kennaly <jkennaly@gmail.com>
2018-08-31 10:34:01 -0700

    configure db access for heroku

--- server/server.js
+++ server/server.js
@@ -5,9 +5,12 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+var MYSQL_CONNECTION_STRING = process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : ''
+
 app.start = function() {
   // start the web server
-  return app.listen(function() {
+   var port = process.env.PORT || 8080;
+  return app.listen(port, function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
