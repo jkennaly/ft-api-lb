0d94ceae8cc0c4540b791ba9b5498f9c2bb03ce1
Jason Kennaly <jkennaly@gmail.com>
2018-08-31 11:09:06 -0700

    configure db access for heroku 4

---
+++ server/datasources.local.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "db": {
+    "name": "db",
+    "connector": "memory"
+  },
+  "DragonAgeFestivalMaster": {
+    "host": "192.168.1.104",
+    "port": 3306,
+    "url": process.env.JAWSDB_URL,
+    "database": "festival_master",
+    "password": "Kt95#8oO!4rzovllpf94",
+    "name": "DragonAgeFestivalMaster",
+    "user": "root",
+    "connector": "mysql"
+  }
+}
