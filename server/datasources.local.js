58e7c766c1d07942c1cdf2ede8dab180f89918b6
Jason Kennaly <jkennaly@gmail.com>
2019-01-06 14:27:04 -0800

    update client

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -3,7 +3,7 @@ let devObject = require('./datasources.json')
 if(process.env.JAWSDB_URL) devObject.DragonAgeFestivalMaster = {
     "url": process.env.JAWSDB_URL + '?connectionLimit=2&debug=false',
     "connector": "mysql",
-    "connectionLimit": 8
+    "connectionLimit": 2
 }
 
 module.exports = devObject
