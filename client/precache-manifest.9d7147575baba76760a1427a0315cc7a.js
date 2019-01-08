6ed187b2a365fe63e943b6ee24931ddac5725480
Jason Kennaly <jkennaly@gmail.com>
2019-01-07 18:36:21 -0800

    update client

---
+++ client/precache-manifest.9d7147575baba76760a1427a0315cc7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21f4dff770fabecf9871",
+    "url": "main.css"
+  },
+  {
+    "revision": "71f2b8122f2a9d58315e79c1cc453cbe",
+    "url": "index.html"
+  },
+  {
+    "revision": "73951d8fb7b813f09d4fd1bd7f3401b0",
+    "url": "img/Crowdshot.jpg"
+  },
+  {
+    "revision": "8ceda9cc1988836b1d45f13aa3371e1d",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "21f4dff770fabecf9871",
+    "url": "bundle.js"
+  }
+];
