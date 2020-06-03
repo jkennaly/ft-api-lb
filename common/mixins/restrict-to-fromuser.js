bc642a0aa8ca0d2c127c5a5f41df14cbd4e70a4a
Jason Kennaly <jkennaly@gmail.com>
2020-06-03 11:46:11 -0700

    bugfixes

--- common/mixins/restrict-to-fromuser.js
+++ common/mixins/restrict-to-fromuser.js
@@ -12,7 +12,7 @@ module.exports = function byUser(Model, options) {
 
     // get current user ID
     const viewerId = Model.app.get('ftUserId')
-    const admin = Model.app.get('scope').includes('admin')
+    const admin = Model.app.get('scope') && Model.app.get('scope').includes('admin')
 
 
 
@@ -22,7 +22,6 @@ module.exports = function byUser(Model, options) {
     if(admin || ctx.data.fromuser === viewerId || !_.isArray(ctx.data)) return next()
     ctx.data = ctx.data.filter(d => viewerId === d.fromuser)
     
-
 
     // next callback in the stack.
     next()
