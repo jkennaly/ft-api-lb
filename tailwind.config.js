54c94d1bc5df49010244dd2c4cf0eefc3bdcc042
Jason Kennaly <jkennaly@gmail.com>
2021-04-21 21:56:19 -0700

    improve site landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        backgroundImage: theme => ({
+
+         'crowd-stage': "url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Crowd_watching_The_Proclaimers%2C_Towersey_2018.jpg/1280px-Crowd_watching_The_Proclaimers%2C_Towersey_2018.jpg')"
+        })
+      },
   },
   variants: {
     extend: {},
